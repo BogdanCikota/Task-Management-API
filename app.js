f25098045a8779027567d318fdbf3f7a3a401e1d
bcikota <bcikota@gmail.com>
2020-12-15 11:27:28 +0100

    Add cors options

--- app.js
+++ app.js
@@ -26,7 +26,20 @@ app.use(methodOverride(function (req, res) {
   }
 }));
 
-app.use(cors());
+const whitelist = ['http://localhost:3000', 'https://shrouded-journey-60588.herokuapp.com/'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions));
 
 const itemSchema = new mongoose.Schema({
   name: String
