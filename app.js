2780a78391442bddaa35e96d9c63849e4868e300
bcikota <bcikota@gmail.com>
2020-12-15 11:55:13 +0100

    Delete cors options

--- app.js
+++ app.js
@@ -26,20 +26,7 @@ app.use(methodOverride(function (req, res) {
   }
 }));
 
-const whitelist = ['http://localhost:3000', 'https://shrouded-journey-60588.herokuapp.com/'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 const itemSchema = new mongoose.Schema({
   name: String
